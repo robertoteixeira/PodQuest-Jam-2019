7b7b0bcfc46464629a9036aefaf06c0fd2efd312
DNPidgey <33699721+DNPidgey@users.noreply.github.com>
2019-09-17 23:39:11 -0300

    17/09¹19 - 23:38
    Colocação de mais props do cenário. Luzes. Animações de dia e noite.

---
+++ NoirCity_PQJam2019/Assets/Models/Debug/Time2x.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Time2x : MonoBehaviour
+{
+	public float speedX = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Time.timeScale = speedX;
+	}
+}
