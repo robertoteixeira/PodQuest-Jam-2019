4b8d0a8ef5bdc739ac93769ca5fbc222f9d4dae7
Roberto Teixeira <roberto.teixeirafs@gmail.com>
2019-09-21 17:40:59 -0300

    Player animator added.

---
+++ NoirCity_PQJam2019/Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    public Animator Anim;
+
+    private PlayerController _playerController;
+    
+    void Start()
+    {
+        _playerController = GetComponent<PlayerController>();
+        _playerController.OnSetCameraMode += OnSetCameraMode;
+    }
+
+    private void OnSetCameraMode(bool status)
+    {
+        Anim.SetBool("cameraOn", status);
+    }
+    
+}
