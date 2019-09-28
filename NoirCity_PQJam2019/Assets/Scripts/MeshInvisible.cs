44c470dd349cdc14cb2a636113dbbbad7d04fc78
Roberto Teixeira <roberto.teixeirafs@gmail.com>
2019-09-28 20:22:42 -0300

    update

---
+++ NoirCity_PQJam2019/Assets/Scripts/MeshInvisible.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshInvisible : MonoBehaviour
+{
+    public GameObject MeshToHide;
+
+    private PlayerController _playerController;
+
+    void Start()
+    {
+        _playerController = GetComponent<PlayerController>();
+        _playerController.OnSetCameraMode += (status) =>
+        {
+            StartCoroutine(Hide(status));
+        };
+    }
+
+    private IEnumerator Hide(bool status)
+    {
+        if (status)
+        {
+            yield return new WaitForSeconds(TimeSpan.FromMilliseconds(2000).Seconds);
+            MeshToHide.SetActive(false);
+        }
+        else
+        {            
+            MeshToHide.SetActive(true);
+        }            
+    }
+}
