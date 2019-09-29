c984dd46cfb07cec36ff7231130d4addcf49443d
Roberto Teixeira <roberto.teixeirafs@gmail.com>
2019-09-29 08:35:20 -0300

    update

---
+++ NoirCity_PQJam2019/Assets/Scripts/CursorManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class CursorManager : Singleton<CursorManager>
+{
+    public Action OnShowCursor;
+    public Action OnHideCursor;
+
+    public void ShowCursor()
+    {
+        Cursor.visible = true;
+        OnShowCursor?.Invoke();
+    }
+
+    public void HideCursor()
+    {
+        Cursor.visible = false;
+        OnHideCursor?.Invoke();
+    }
+}
--- NoirCity_PQJam2019/Assets/Scripts/MainMenu.cs
+++ NoirCity_PQJam2019/Assets/Scripts/MainMenu.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
@@ -8,6 +6,6 @@ public class MainMenu : MonoBehaviour
     public void LoadGame()
     {
         SceneManager.LoadScene(1);
-        AudioManager.Instance.PlayAmbient();
+        AudioManager.Instance.PlayAmbient();        
     }
 }
