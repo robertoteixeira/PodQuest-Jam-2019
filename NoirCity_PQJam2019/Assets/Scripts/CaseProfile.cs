3f3960d38980c627d24fa585db15e352efb5ea66
Roberto Teixeira <roberto.teixeirafs@gmail.com>
2019-09-29 12:01:37 -0300

    Statusmanager

---
+++ NoirCity_PQJam2019/Assets/Scripts/CaseProfile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class CaseProfile
+{
+    [SerializeField]
+    public string Text;
+    [SerializeField]
+    public bool Conclusive;    
+}
---
+++ NoirCity_PQJam2019/Assets/Scripts/Managers/StatusManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class StatusManager : MonoBehaviour
+{
+    public int CurrentStatus;
+    public CaseProfile[] Profiles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
